05f35409518c55a72d33d699b7d08be6e001c72a
cse-manideep-polusani <cse.manideep.polusani@gmail.com>
2026-02-21 14:09:44 +0530

    Add SpringCoreTask1 and SpringCoreTask2 to Tasks branch

---
+++ SpringCoreTask1/src/main/java/com/payment/demo/SpringCoreTask1Application.java
@@ -0,0 +1,13 @@
+package com.payment.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreTask1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoreTask1Application.class, args);
+	}
+
+}
---
+++ SpringCoreTask2/src/main/java/com/payment/demo/SpringCoreTask2Application.java
@@ -0,0 +1,13 @@
+package com.payment.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreTask2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoreTask2Application.class, args);
+	}
+
+}
